1cddf98b2633c854632315620d294da822e79c44
Rainer Sigwald <raines@microsoft.com>
2016-11-22 13:17:08 -0600

    WIP

---
+++ src/XMakeBuildEngine/Evaluation/ImplicitImportList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Build.Evaluation
+{
+    class ImplicitImportList
+    {
+        public IList<string> ImportPathFolders
+        {
+            get
+            {
+                return
+                    !String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("MSBUILDMAGICIMPORTFOLDER"))
+                        ? new List<String> {Environment.GetEnvironmentVariable("MSBUILDMAGICIMPORTFOLDER")}
+                        : new List<string>();
+            }
+        }
+    }
+}
