8bf73fa4ce1675d574e653a824ad83bc562900cc
Rainer Sigwald <raines@microsoft.com>
2018-03-08 17:30:37 -0800

    debug bits

--- makepackage.cmd
+++ makepackage.cmd
@@ -1,12 +1,12 @@
 set RepoRoot=%~dp0
 set RepoRoot=%RepoRoot:~0,-1%
 
-call cibuild.cmd --scope build --config Release --bootstrap-only
+call cibuild.cmd --scope build --config Debug --bootstrap-only
 
 rd /s /q bin\Bootstrap\MSBuild\15.0\Bin\SdkResolvers\NuGet.MSBuildSdkResolver
 
-nuget.exe pack setup\MsBuild.Engine.Corext\MsBuild.Engine.Corext.nuspec -NonInteractive -OutputDirectory bin\Setup -Properties version=%1;repoRoot=%RepoRoot%;X86BinPath=%~dp0bin\Release\x86\Windows_NT\Output;X64BinPath=%~dp0bin\Release\x64\Windows_NT\Output -Verbosity Detailed
+nuget.exe pack setup\MsBuild.Engine.Corext\MsBuild.Engine.Corext.nuspec -NonInteractive -OutputDirectory bin\Setup -Properties version=%1;repoRoot=%RepoRoot%;X86BinPath=%~dp0bin\Debug\x86\Windows_NT\Output;X64BinPath=%~dp0bin\Release\x64\Windows_NT\Output -Verbosity Detailed
 
 md \\fsu\shares\MsEng\MSBuild\raines\dd_558508\%1
 
-robocopy /mt:128 C:\src\msbuild\bin\Release\x64\Windows_NT\Output\ \\fsu\shares\MsEng\MSBuild\raines\dd_558508\%1\ Microsoft.Build*dll Microsoft.Build*pdb MSBuild.dll MSBuild.pdb MSBuild.exe /xf *unittests* /xf Microsoft.Build.Tasks.CodeAnalysis.dll
+robocopy /mt:128 C:\src\msbuild\bin\Debug\x86\Windows_NT\Output\ \\fsu\shares\MsEng\MSBuild\raines\dd_558508\%1\ Microsoft.Build*dll Microsoft.Build*pdb MSBuild.dll MSBuild.pdb MSBuild.exe /xf *unittests* /xf Microsoft.Build.Tasks.CodeAnalysis.dll
