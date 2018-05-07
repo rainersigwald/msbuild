e75a0cd328dc69e4ebeee0f59a5f640237b3f8dd
Rainer Sigwald <raines@microsoft.com>
2018-05-07 13:01:00 -0500

    create classes to hold RAR request/response

---
+++ src/Tasks/AssemblyDependency/ResolveAssemblyReferenceResponse.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+
+namespace Microsoft.Build.Tasks.AssemblyDependency
+{
+    class ResolveAssemblyReferenceResponse
+    {
+        public ITaskItem[] CopyLocalFiles { get; set; }
+        public string DependsOnNETStandard { get; set; }
+        public string DependsOnSystemRuntime { get; set; }
+        public ITaskItem[] FilesWritten { get; set; }
+        public ITaskItem[] RelatedFiles { get; set; }
+        public ITaskItem[] ResolvedDependencyFiles { get; set; }
+        public ITaskItem[] ResolvedFiles { get; set; }
+        public ITaskItem[] ResolvedSDKReferences { get; set; }
+        public ITaskItem[] SatelliteFiles { get; set; }
+        public ITaskItem[] ScatterFiles { get; set; }
+        public ITaskItem[] SuggestedRedirects { get; set; }
+
+    }
+}
