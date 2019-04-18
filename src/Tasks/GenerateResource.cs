a705b9fc626c59b5b165460e59a599d8145f6363
Rainer Sigwald <raines@microsoft.com>
2019-04-18 13:21:59 -0500

    Read byte[] data from resx (hackily)

--- src/Tasks/GenerateResource.cs
+++ src/Tasks/GenerateResource.cs
@@ -3659,11 +3659,36 @@ namespace Microsoft.Build.Tasks
                 {
                     xmlReader.WhitespaceHandling = WhitespaceHandling.None;
                     XDocument doc = XDocument.Load(xmlReader, LoadOptions.PreserveWhitespace);
+
+                    // TODO: support type alias resolution
+
                     foreach (XElement dataElem in doc.Element("root").Elements("data"))
                     {
                         string name = dataElem.Attribute("name").Value;
                         string value = dataElem.Element("value").Value;
-                        AddResource(reader, name, value, filename);
+
+                        if (dataElem.Attribute("type") is null)
+                        {
+                            // No type means "string"
+                            AddResource(reader, name, value, filename);
+                            continue;
+                        }
+
+                        if (dataElem.Attribute("type").Value == "System.Drawing.Bitmap, System.Drawing")
+                        {
+                            var e = new Entry(name, null)
+                            {
+                                hasSerializedData = true,
+                                typeName = "System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
+                                serializedData = Convert.FromBase64String(value),
+                            };
+
+                            AddResource(reader, filename, 0, 0, e);
+                        }
+                        else
+                        {
+                            throw new NotImplementedException("should probably support other types, Rainer");
+                        }
                     }
                 }
             }
@@ -3779,14 +3804,19 @@ namespace Microsoft.Build.Tasks
         {
             Entry entry = new Entry(name, value);
 
-            if (reader.resourcesHashTable.ContainsKey(name))
+            AddResource(reader, inputFileName, lineNumber, linePosition, entry);
+        }
+
+        private void AddResource(ReaderInfo reader, string inputFileName, int lineNumber, int linePosition, Entry entry)
+        {
+            if (reader.resourcesHashTable.ContainsKey(entry.name))
             {
-                _logger.LogWarningWithCodeFromResources(null, inputFileName, lineNumber, linePosition, 0, 0, "GenerateResource.DuplicateResourceName", name);
+                _logger.LogWarningWithCodeFromResources(null, inputFileName, lineNumber, linePosition, 0, 0, "GenerateResource.DuplicateResourceName", entry.name);
                 return;
             }
 
             reader.resources.Add(entry);
-            reader.resourcesHashTable.Add(name, value);
+            reader.resourcesHashTable.Add(entry.name, entry.value);
         }
 
         /// <summary>
@@ -3959,6 +3989,10 @@ namespace Microsoft.Build.Tasks
 
             public string name;
             public object value;
+
+            public bool hasSerializedData;
+            public string typeName;
+            public byte[] serializedData;
         }
 #endregion // Code from ResGen.EXE
     }
