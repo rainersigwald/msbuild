1bd5bff28aab365a324b32ab3f71987a38869738
Rainer Sigwald <raines@microsoft.com>
2021-10-20 17:35:43 -0500

    WIP moving shared from engine to Framework
    Allllllllmost working, but AssemblyResources is going to be a bear--it means a different thing in each assembly (try local resources and fall back to shared, with 'local' varying). I _think_ that we can just leave it everywhere and define a new shared?? maybe???

---
+++ src/Framework/AssemblyResources.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+
+namespace Microsoft.Build.Shared
+{
+    /// <summary>
+    /// This class provides access to the assembly's resources.
+    /// </summary>
+    static class SharedResources
+    {
+        /// <summary>
+        /// Loads the specified resource string, either from the assembly's primary resources, or its shared resources.
+        /// </summary>
+        /// <remarks>This method is thread-safe.</remarks>
+        /// <param name="name"></param>
+        /// <returns>The resource string, or null if not found.</returns>
+        internal static string GetString(string name)
+        {
+            // NOTE: the ResourceManager.GetString() method is thread-safe
+            string resource = s_sharedResources.GetString(name, CultureInfo.CurrentUICulture);
+
+            ErrorUtilities.VerifyThrow(resource != null, "Missing resource '{0}'", name);
+
+            return resource;
+        }
+
+        // shared resources
+        private static readonly ResourceManager s_sharedResources = new ResourceManager("MSBuild.Strings.shared", typeof(SharedResources).GetTypeInfo().Assembly);
+    }
+}
